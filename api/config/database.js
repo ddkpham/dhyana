e8ef7a6682fd371d1403b6bfc93f798d6e2a475e
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-07-18 15:24:45 -0700

    update docker-compose

--- api/config/database.js
+++ api/config/database.js
@@ -2,14 +2,16 @@ const Sequelize = require("sequelize");
 // TODO: local user atm. change to more general user later
 
 console.log("DB Host IP: ", process.env.DB_HOST);
+console.log("process.env.POSTGRES_USER", process.env.POSTGRES_USER);
 module.exports = new Sequelize(
-  process.env.TEST_DB || "dhyanadb",
+  process.env.TEST_DB || "dyhana",
   // "davidpham", // local connection
-  process.env.POSTGRES_USER || "davidpham",
+  process.env.POSTGRES_USER || "postgres",
   "postgres",
   {
     host: process.env.DB_HOST || "localhost",
     dialect: "postgres",
+    port: 5432,
 
     pool: {
       max: 5,
