622500654ec67199ba4a14e65494110b5147ffbb
sbarer <sbarer@sfu.ca>
2020-07-18 15:24:10 -0700

    Created basic functionality page for create user. waiting for backend

---
+++ client/src/components/CreateUser/index.js
@@ -0,0 +1,74 @@
+import { baseURL, clientBaseURL } from "../../config/settings";
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import "./index.scss";
+
+
+function CreateUser() {
+  const [username, setUsername] = useState("");
+  const [pass, setPass] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const login = async (user, name) => {
+    const url = `${baseURL}/createUser`;
+    const body = { username, pass, firstName, lastName };
+    const response = await fetch(url, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+    const { confirmation } = data;
+    if (confirmation == "success") {
+      localStorage.setItem("auth-token", "success");
+
+      window.location.href = `${clientBaseURL}/home`;
+    }
+  };
+
+  return (
+    <div id="mainDiv">
+      <h2>Create New User</h2>
+      <div className="text-input">
+        <TextField
+          label="username"
+          variant="outlined"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <TextField
+          label="password"
+          variant="outlined"
+          onChange={(event) => {
+            setPass(event.target.value);
+          }}
+        />
+        <TextField
+          label="firstName"
+          variant="outlined"
+          onChange={(event) => {
+            setFirstName(event.target.value);
+          }}
+        />
+        <TextField
+          label="lastName"
+          variant="outlined"
+          onChange={(event) => {
+            setLastName(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <Button variant="outlined" color="primary" onClick={login}>
+          Create User
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default CreateUser;
