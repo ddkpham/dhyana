12770851bb2dae91991eb1e749f171cf25188fb9
ppa21 <pruthviraj_patel@sfu.ca>
2020-07-20 18:49:25 -0700

    included some sanitization

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,25 +1,43 @@
+const { body, validationResult } = require("express-validator");
 var User = require("../models/User");
 
 exports.create_new_user = function (req, res, next) {
   console.log(req.body);
-  const { username, password, first_name, last_name } = req.body;
 
-  User.create({
-    first_name,
-    last_name,
-    username,
-    password,
-  })
-    .then((user) => {
-      res.json({
-        confirmation: "User created successfully.",
-        data: user,
-      });
+  body(req.body).trim().escape().not().isEmpty();
+
+  user = req.body.username.trim();
+  pass = req.body.password.trim();
+  const errors = validationResult(req.body);
+
+  if (!errors.isEmpty()) {
+    res.send("Error(s) in the inputted data or data format.");
+    res.next();
+  } else {
+    if (user == "" || pass == "") {
+      res.send("Username/Password can't be null.");
+      return next();
+    }
+
+    const { username, password, first_name, last_name } = req.body;
+
+    User.create({
+      first_name,
+      last_name,
+      username,
+      password,
     })
-    .catch((err) => {
-      res.json({
-        confirmation: "User already exists.",
-        err: err,
+      .then((user) => {
+        res.json({
+          confirmation: "User created successfully.",
+          data: user,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          confirmation: "User already exists.",
+          err: err,
+        });
       });
-    });
+  }
 };
