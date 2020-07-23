a40efa513975dd7d9bc0fd23ca5bd2da6d991046
Toria Kindersley <toria.kindersley@thrive.health>
2020-07-23 16:11:41 -0700

    Fix team route collision

--- api/routes/team.js
+++ api/routes/team.js
@@ -5,12 +5,12 @@ const { body, param } = require("express-validator");
 
 router.post("/create", teamController.create_team);
 
+router.get("/all", teamController.view_all);
+
 router.get(
   "/:name",
   [param("name").isLength({ min: 2 })],
   teamController.view_team
 );
-
-router.get("/all", teamController.view_all);
 
 module.exports = router;
