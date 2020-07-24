cd55fdca908db11f63ea5966fee2a318e0ca2b05
Toria Kindersley <toria.kindersley@thrive.health>
2020-07-23 20:03:33 -0700

    Clean up task file

--- client/src/components/Task/index.js
+++ client/src/components/Task/index.js
@@ -1,37 +1,8 @@
-// import React from "react";
-// import Typography from '@material-ui/core/Typography';
+import React, { Fragment, useState, useRef } from "react";
+import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-// import { useDrag } from 'react-dnd'
-
-// function Task({task}){
-//     const [{isDragging}, drag] = useDrag({
-//         item: { type: 'card' }, //TODO: move into const file
-//         collect: monitor => ({
-//             isDragging: !!monitor.isDragging(),
-//         }),
-//     });
-
-//     if(isDragging) return null
-
-//     return (
-//         <div ref={drag}>
-//         <Card raised>
-//             <CardActionArea>
-//                 <CardContent>
-//                     <Typography variant="body1" gutterBottom>{task.name}</Typography>
-//                     <Typography variant="body2">{task.description}</Typography>
-//                 </CardContent>
-//             </CardActionArea>
-//         </Card>
-//         </div>
-//     );
-// }
-
-// export default Task;
-
-import React, { Fragment, useState, useRef } from "react";
 import { useDrag, useDrop } from "react-dnd";
 
 const Task = ({ task, index, moveItem }) => {
@@ -39,32 +10,6 @@ const Task = ({ task, index, moveItem }) => {
 
     const [, drop] = useDrop({
         accept: "card",
-        hover(task, monitor) {
-            if (!ref.current) {
-                return
-            }
-            const dragIndex = task.index;
-            const hoverIndex = index;
-
-            if (dragIndex === hoverIndex) {
-                return
-            }
-
-            const hoveredRect = ref.current.getBoundingClientRect();
-            const hoverMiddleY = (hoveredRect.bottom - hoveredRect.top) / 2;
-            const mousePosition = monitor.getClientOffset();
-            const hoverClientY = mousePosition.y - hoveredRect.top;
-
-            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-                return;
-            }
-
-            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-                return;
-            }
-            moveItem(dragIndex, hoverIndex);
-            task.index = hoverIndex;
-        },
     });
 
     const [{ isDragging }, drag] = useDrag({
@@ -84,8 +29,12 @@ const Task = ({ task, index, moveItem }) => {
             ref={ref}
             style={{ opacity: isDragging ? 0 : 1 }}
         >
-                <p>{task.name}</p>
-                <p>{task.description}</p>
+            <CardActionArea>
+                <CardContent>
+                    <Typography variant="body1" gutterBottom>{task.name}</Typography>
+                    <Typography variant="body2">{task.description}</Typography>
+                </CardContent>
+            </CardActionArea>
         </Card>
     );
 };
