2597abb038bfdcc36959cddf5f26a47f9094cfb4
ppa21 <pruthviraj_patel@sfu.ca>
2020-07-31 14:45:28 -0700

    changed task model

--- api/models/Task.js
+++ api/models/Task.js
@@ -9,6 +9,39 @@ const Task = db.define("task", {
   description: {
     type: Sequelize.STRING,
   },
+  // activity_log: {
+  //   type: Sequelize.ARRAY[String],
+  // },
+  user_id_create: {
+    type: Sequelize.INTEGER,
+  },
+  date_created: {
+    type: Sequelize.DATE,
+  },
+  priority: {
+    type: Sequelize.INTEGER,
+  },
+  time_estimated: {
+    type: Sequelize.DOUBLE,
+  },
+  time_elapsed: {
+    type: Sequelize.DOUBLE,
+  },
+  user_id_assigned: {
+    type: Sequelize.INTEGER,
+  },
+  flag: {
+    type: Sequelize.BOOLEAN,
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  date_modified: {
+    type: Sequelize.DATE,
+  },
+  num_changes: {
+    type: Sequelize.INTEGER,
+  },
 });
 
 module.exports = Task;
