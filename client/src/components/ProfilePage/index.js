ff92ee22657a77762f3d725b065635c8b0d11878
daniel <dkorin@sfu.ca>
2020-08-02 13:52:52 -0700

    added route to add a user to team, added team selector drop down in profile page, enabled session user to be added to a team when a team is created

--- client/src/components/ProfilePage/index.js
+++ client/src/components/ProfilePage/index.js
@@ -1,14 +1,32 @@
 import { baseURL } from "../../config/settings";
 import React, { useState, useEffect } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import Button from "@material-ui/core/Button";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 import "./index.scss";
 import { useHistory } from "react-router-dom";
 import { getCall } from "../../apiCalls/apiCalls";
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 function ProfilePage(props) {
+
+  const classes = useStyles();
   const { username } = props;
   let history = useHistory();
   const [userInfo, setUser] = useState([]);
+  const [teamInfo, showTeams] = useState([]);
 
   useEffect(() => {
     function getUser() {
@@ -29,6 +47,24 @@ function ProfilePage(props) {
     console.log("userInfo", userInfo);
   }, [userInfo]);
 
+  useEffect(() => {
+    function getTeams() {
+      const url = `${baseURL}/team/all`;
+      getCall(url)
+        .then((response) => response.json())
+        .then((payload) => {
+          console.log("payload", payload);
+          showTeams(payload.data);
+        })
+        .catch((err) => console.log("project fetch error", err));
+    }
+    getTeams();
+  }, []);
+
+  useEffect(() => {
+    console.log("teamInfo", teamInfo);
+  }, [teamInfo]);
+
   return (
     <div>
       <div className="buttonDiv">
@@ -48,6 +84,17 @@ function ProfilePage(props) {
           <h2 className="name">
             Name: {userInfo.first_name} {userInfo.last_name}
           </h2>
+          <FormControl className={classes.formControl} variant="outlined">
+          <InputLabel>Team</InputLabel>
+          <Select
+            label="Team"
+          >
+            <MenuItem>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem>Ten</MenuItem>
+          </Select>
+        </FormControl>
         </div>
       </div>
     </div>
