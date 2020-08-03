afeba1566f142d0e4d6953a97cfd05b32f8464c1
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-08-03 11:37:06 -0700

    update user feedback messages

--- client/src/components/ProfilePage/index.js
+++ client/src/components/ProfilePage/index.js
@@ -1,11 +1,11 @@
 import { baseURL, clientBaseURL } from "../../config/settings";
 import React, { useState, useEffect } from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
 import "./index.scss";
 import { useHistory } from "react-router-dom";
 import { postCall, getCall } from "../../apiCalls/apiCalls";
@@ -21,7 +21,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function ProfilePage(props) {
-
   const classes = useStyles();
   const { username } = props;
   let history = useHistory();
@@ -41,11 +40,12 @@ function ProfilePage(props) {
 
     const data = await response.json();
     const { confirmation, message } = data;
-    console.log(data)
+    console.log(data);
     if (confirmation === "success") {
+      alert(`Success: ${message}`);
       window.location.href = `${clientBaseURL}/home`;
     } else {
-      alert(message)
+      alert(`Error: ${message}`);
     }
   };
 
@@ -106,27 +106,28 @@ function ProfilePage(props) {
             Name: {userInfo.first_name} {userInfo.last_name}
           </h2>
           <FormControl className={classes.formControl} variant="outlined">
-          <InputLabel>Team</InputLabel>
-          <Select
-            label="Team"
-            onChange={(event) => {
-              getTeamId(event.target.value);
-            }}
-          >
-            <MenuItem>
-              <em>None</em>
-            </MenuItem>
-            {
-              teamInfo.map((info) =>
-              <MenuItem value={info.id} key={info.id}>{info.name}</MenuItem>
-            )}
-          </Select>
-        </FormControl>
-        <div>
-          <Button variant="outlined" color="primary" onClick={addUserToTeam}>
-            Add User to Team
-          </Button>
-        </div>
+            <InputLabel>Team</InputLabel>
+            <Select
+              label="Team"
+              onChange={(event) => {
+                getTeamId(event.target.value);
+              }}
+            >
+              <MenuItem>
+                <em>None</em>
+              </MenuItem>
+              {teamInfo.map((info) => (
+                <MenuItem value={info.id} key={info.id}>
+                  {info.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <div>
+            <Button variant="outlined" color="primary" onClick={addUserToTeam}>
+              Add User to Team
+            </Button>
+          </div>
         </div>
       </div>
     </div>
