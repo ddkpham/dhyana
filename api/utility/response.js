4846dbcb6f0ff45e789c92141484962de34443a5
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-08-03 14:49:06 -0700

    update api response object

--- api/utility/response.js
+++ api/utility/response.js
@@ -3,12 +3,14 @@ const errorResponse = (message, err = {}) => ({
   data: [],
   message,
   err,
+  success: false,
 });
 
 const successResponse = (message, data = []) => ({
   confirmation: "success",
   data,
   message,
+  success: true,
 });
 
 module.exports = { errorResponse, successResponse };
