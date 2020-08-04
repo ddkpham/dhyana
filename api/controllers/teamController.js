5d3cda3fad1d29232d88e0d89480aab8a8297028
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-08-04 14:56:13 -0700

    Update get team info route to have project info
    team info object will contain a project list and in each
    project object, there will be a column object with a brief
    summary of tasks

--- api/controllers/teamController.js
+++ api/controllers/teamController.js
@@ -2,6 +2,10 @@ const { body, validationResult } = require("express-validator");
 const Team = require("../models/Team");
 const TeamsUsers = require("../models/TeamsUsers");
 var User = require("../models/User");
+var Project = require("../models/Project");
+var ProjectColumns = require("../models/ProjectColumns");
+var ColumnsTasks = require("../models/ColumnsTasks");
+var Column = require("../models/Column");
 const { Op } = require("sequelize");
 
 const { errorResponse, successResponse } = require("../utility/response");
@@ -12,8 +16,8 @@ exports.create_team = function (req, res, next) {
   body(req.body).trim().escape().not().isEmpty();
   const name = req.body.name.trim();
 
-  const errors = validationResult(req.body);
   if (!errors.isEmpty()) {
+    const errors = validationResult(req.body);
     res.status(400).json(errorResponse("errors in inputted data"));
     return;
   }
@@ -39,48 +43,47 @@ exports.get_users = function (req, res, next) {
   console.log("exports.get_user -> req.params", req.params);
   const team_id = req.params.id.trim();
 
-  TeamsUsers.findAll ({
+  TeamsUsers.findAll({
     where: {
       team_id: team_id,
     },
     attributes: [`user_id`],
-    },
-  )
+  })
     .then((data) => {
-      var usersArray = []
+      var usersArray = [];
       for (var i = 0; i < data.length; i++) {
-        const { dataValues: { user_id : userId } } = data[i]
-        console.log("userId is:", i, userId)
-        usersArray.push(userId)
+        const {
+          dataValues: { user_id: userId },
+        } = data[i];
+        console.log("userId is:", i, userId);
+        usersArray.push(userId);
       }
 
       if (!usersArray.length) {
         res.status(400).json(errorResponse("Didn't find any users in Team"));
         return;
-      } 
+      }
 
       User.findAll({
         where: {
           id: {
-            [Op.in]: usersArray
-          }
+            [Op.in]: usersArray,
+          },
         },
         attributes: [`id`, `username`, `first_name`, `last_name`],
-        },
-      )
-      .then((users) => {
+      }).then((users) => {
         if (users.length > 0) {
           res.status(200).json(successResponse("user(s) found", users));
           return;
         }
-  
+
         return Promise.reject();
-      })
+      });
     })
     .catch((err) => {
       res.status(404).json(errorResponse("Didn't find any users in Team", err));
     });
-}
+};
 
 exports.add_user = function (req, res, next) {
   console.log("exports.add_user -> req.body", req.body);
@@ -150,22 +153,103 @@ exports.view_team = function (req, res, next) {
     return;
   }
 
-  Team.findAll({
-    where: {
-      name,
-    },
-  })
-    .then((team) => {
-      console.log("exports.view_team -> team", team);
+  var teamInfo = {};
+
+  const getTeamInfo = async () => {
+    try {
+      const team = await Team.findAll({
+        where: {
+          name,
+        },
+      });
+
+      // get basic team info
       if (team.length) {
-        res.status(200).json(successResponse("Sucessfully found team", team));
+        teamInfo = team[0].dataValues;
+        console.log("getTeamInfo -> teamInfo", teamInfo);
+
+        // get all projects that team owns, building query for project stats preview
+        const projects = await Project.findAll({
+          where: {
+            team_id: teamInfo.id,
+          },
+        });
+
+        // get columns for each project
+        const projectColumns = await ProjectColumns.findAll({
+          where: {
+            [Op.or]: projects.map((p) => ({ project_id: p.dataValues.id })),
+          },
+        });
+
+        // for each column, grab tasks in column
+        const columnTasks = await ColumnsTasks.findAll({
+          where: {
+            [Op.or]: projectColumns.map((p) => ({
+              column_id: p.dataValues.column_id,
+            })),
+          },
+        });
+
+        // create column stats by summing up num of tasks
+        const columnCountMap = {};
+        columnTasks.forEach((c) => {
+          const {
+            dataValues: { column_id },
+          } = c;
+          if (!columnCountMap[`${column_id}`]) {
+            columnCountMap[`${column_id}`] = 1;
+          } else {
+            columnCountMap[`${column_id}`] = columnCountMap[`${column_id}`] + 1;
+          }
+        });
+
+        // append column stats to each column
+        const columns = await Column.findAll({
+          where: {
+            [Op.or]: columnTasks.map((c) => ({ id: c.dataValues.column_id })),
+          },
+        });
+
+        const columnInfo = columns.map((c) => {
+          const info = c.dataValues;
+          info.numTasks = columnCountMap[`${c.dataValues.id}`];
+          return info;
+        });
+
+        // attach column info to respective projects
+        const projectInfo = projects.map((p) => {
+          const info = p.dataValues;
+          info.columns = [];
+          // if project id matches pcolumn id, add column info to columninfo array
+          projectColumns.forEach((pcolumns) => {
+            if (pcolumns.dataValues.project_id == p.dataValues.id) {
+              // grab column id and column info
+              const cInfo = columnInfo.filter(
+                (c) => c.id == pcolumns.dataValues.column_id
+              )[0];
+              if (cInfo) {
+                info.columns.push(cInfo);
+              }
+            }
+          });
+          return info;
+        });
+
+        // finally attach to team info object
+        teamInfo.projects = projectInfo;
+
+        res
+          .status(200)
+          .json(successResponse("Sucessfully found team", teamInfo));
       } else {
-        res.status(404).json(errorResponse("Team not found.", err));
+        res.status(200).json(errorResponse("Team not found."));
       }
-    })
-    .catch((err) => {
-      res.status(404).json(errorResponse("Team not found.", err));
-    });
+    } catch (err) {
+      res.status(404).json(errorResponse("Error in fetching team info.", err));
+    }
+  };
+  getTeamInfo();
 };
 
 exports.view_all = function (req, res, next) {
