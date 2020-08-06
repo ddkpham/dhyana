81a0ec8b3c2801ed70ad912978fdd83be6d37f0a
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-08-06 15:39:00 -0700

    update profile page for users with new data
    biography and job title added to user profile. I have updated this
    in both my profile and user profile. In addition, I've also spruced
    up the styling.

--- client/src/components/MyProfile/index.js
+++ client/src/components/MyProfile/index.js
@@ -2,12 +2,19 @@ import { baseURL } from "../../config/settings";
 import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import "./index.scss";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Typography from "@material-ui/core/Typography";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+
 import { useHistory } from "react-router-dom";
 import { getCall } from "../../apiCalls/apiCalls";
 
 function MyProfile() {
   let history = useHistory();
-  const [ProfileInfo, setProfile] = useState([]);
+  const [ProfileInfo, setProfile] = useState({});
 
   useEffect(() => {
     function getProfile() {
@@ -42,13 +49,42 @@ function MyProfile() {
         </Button>
       </div>
 
-      <div className="container">
-        <div className="profileDiv">
-          <h2 className="username">Username: {ProfileInfo.username}</h2>
-          <h2 className="name">
-            Name: {ProfileInfo.first_name} {ProfileInfo.last_name}
-          </h2>
-        </div>
+      <div className="profile-container">
+        <Card className="profile-profileDiv">
+          <CardContent>
+            <AccountBoxIcon />
+            <Typography variant="h5" color="primary">
+              Username: {ProfileInfo.username}
+            </Typography>
+            <Typography variant="h6" color="secondary">
+              first name:{" "}
+              {ProfileInfo.first_name ? ProfileInfo.first_name : "None of"}
+            </Typography>
+            <Typography variant="h6" color="secondary">
+              last name:{" "}
+              {ProfileInfo.last_name ? ProfileInfo.last_name : "Your business"}
+            </Typography>
+            <Typography variant="h6" color="secondary">
+              job title:{" "}
+              {ProfileInfo.job_title
+                ? ProfileInfo.job_title
+                : "CEO of mind your own"}
+            </Typography>
+            <Typography variant="h6" color="secondary">
+              Biography:{" "}
+            </Typography>
+            <TextareaAutosize
+              rowsMax={5}
+              aria-label="maximum height"
+              placeholder="Maximum 4 rows"
+              defaultValue={
+                ProfileInfo.biography
+                  ? ProfileInfo.biography
+                  : "Please respect my privacy. I'm a private person."
+              }
+            />
+          </CardContent>
+        </Card>
       </div>
 
       <div className="container2">
@@ -58,7 +94,7 @@ function MyProfile() {
             variant="outlined"
             color="primary"
             onClick={() => {
-              history.push("/editProfile");
+              history.push("/edit-profile");
             }}
           >
             Edit Profile
