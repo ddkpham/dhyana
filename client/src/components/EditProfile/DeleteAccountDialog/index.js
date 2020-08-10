542500d7ee5e1a51e510c74d16ad33ba6b30208e
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-08-10 09:58:53 -0700

    log user out of session and redirect to login page
    successful account deletion links user back to home page.
    Clears session token as well.

--- client/src/components/EditProfile/DeleteAccountDialog/index.js
+++ client/src/components/EditProfile/DeleteAccountDialog/index.js
@@ -15,12 +15,12 @@ import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import SentimentDissatisfiedIcon from "@material-ui/icons/SentimentDissatisfied";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import { postCall } from "../../../apiCalls/apiCalls";
+import { postCall, getCall } from "../../../apiCalls/apiCalls";
 import { baseURL } from "../../../config/settings";
+import { useHistory } from "react-router-dom";
 
 import { blue } from "@material-ui/core/colors";
 
-const emails = ["username@gmail.com", "user02@gmail.com"];
 const useStyles = makeStyles({
   avatar: {
     backgroundColor: blue[100],
@@ -32,14 +32,23 @@ function SimpleDialog(props) {
   const classes = useStyles();
   const [username, setUsername] = useState("");
   const { onClose, selectedValue, open, user } = props;
+  const history = useHistory();
   console.log("SimpleDialog -> user", user);
 
   const handleClose = () => {
     onClose();
   };
 
-  const handleListItemClick = (value) => {
-    // onClose(value);
+  const logout = async () => {
+    const url = `${baseURL}/login/clear-cookie`;
+    const response = await getCall(url);
+    const data = await response.json();
+    const { confirmation } = data;
+    if (confirmation === "success") {
+      localStorage.removeItem("auth-token");
+    } else {
+      console.log("failed to clear session cookie");
+    }
   };
 
   const deleteAccount = async () => {
@@ -51,10 +60,21 @@ function SimpleDialog(props) {
       return;
     }
 
+    // log user out
+
     const url = `${baseURL}/user/delete`;
     const body = { id: user.id };
     const response = await postCall(url, body);
     const payload = await response.json();
+
+    if (payload.confirmation == "success") {
+      alert("good bye friend 👋");
+      await logout(); // log user out
+      history.push("/home");
+      window.location.reload(false);
+    } else {
+      alert("error occurred in account deletion");
+    }
     console.log("deleteAccount -> payload", payload);
   };
 
