481bd51d8f726d90f1482b6f0d3b9bc280db7f54
David Pham <davidpham@Davids-MacBook-Pro.local>
2020-08-09 18:11:29 -0700

    add route to delete user from team

--- api/routes/team.js
+++ api/routes/team.js
@@ -7,6 +7,12 @@ router.post("/create", teamController.create_team);
 
 router.post("/delete", [body("id")], teamController.delete_team);
 
+router.post(
+  "/delete/user",
+  [body("user_id"), body("team_id")],
+  teamController.delete_user_from_team
+);
+
 router.get("/all", teamController.view_all);
 
 router.get(
